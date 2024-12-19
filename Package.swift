429ad22971281e55355dbc32efb958c3faee5452
박경우 <devgster@gmail.com>
2024-12-19 14:58:37 +0900

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "CRRefresh",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "CRRefresh",
+            targets: ["CRRefresh"]),
+    ],
+    targets: [
+        .target(
+            name: "CRRefresh",
+            resources: [
+                .copy("Assets/")
+            ]
+        ),
+    ]
+)
